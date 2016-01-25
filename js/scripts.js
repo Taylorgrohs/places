6df67d0bb4996e535242c506460b6c88608d96d6
Jay Whang and Taylor Grohs <taylorgrohs@gmail.com>
2016-01-25 09:37:19 -0800

    pass test 2

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ function Place (locations, year, reason) {
   this.year = year;
   this.reason = reason;
 }
+"You went to Paris in 2015 for fun"
+Place.prototype.together = function() {
+  return "You went to" + " " + this.locations + " "
+ + "in" + " " + this.year + " " + "for" + " " + this.reason}
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,12 @@
 describe('Place', function() {
   it("creates a new Place you have visited", function() {
-    var testContact = new Place("London","2010", "");
-    expect(testContact.locations).to.equal("London");
-    expect(testContact.year).to.equal("2010");
-    expect(testContact.reason).to.equal("");
+    var testPlace = new Place("London","2010", "");
+    expect(testPlace.locations).to.equal("London");
+    expect(testPlace.year).to.equal("2010");
+    expect(testPlace.reason).to.equal("");
+  });
+  it("adds the together method to all Place", function() {
+    var testPlace = new Place("Paris", 2015, "fun");
+    expect(testPlace.together()).to.equal("You went to Paris in 2015 for fun")
   });
 });

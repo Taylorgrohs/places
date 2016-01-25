3317cb692c47a212e8d47982ec86906ee217cc7b
Jay Whang and Taylor Grohs <taylorgrohs@gmail.com>
2016-01-25 10:13:00 -0800

    finish form for places input

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,28 @@ function Place (locations, year, reason) {
 "You went to Paris in 2015 for fun"
 Place.prototype.together = function() {
   return "You went to" + " " + this.locations + " "
- + "in" + " " + this.year + " " + "for" + " " + this.reason}
+ + "in" + " " + this.year + " " + "for" + " " + this.reason;
+};
+
+$(document).ready(function(){
+  $("form#new-place").submit(function(event){
+    var inputtedPlace = $("input#new-place").val();
+    var inputtedYear = parseInt($("input#new-year").val());
+    var inputtedReason = $("input#new-reason").val();
+    var newPlace = new Place(inputtedPlace, inputtedYear, inputtedReason);
+
+    $("ul#place").append("<li><span class='places'>" + newPlace.locations + "</span></li>");
+
+    $("input#new-place").val("");
+    $("input#new-year").val("");
+    $("input#new-reason").val("");
+
+    $(".places").last().click(function(){
+      $("#show-place").show();
+      $("#show-place h2").text("Info: ");
+      $(".info").text(newPlace.together());
+    });
+
+    event.preventDefault();
+  });
+});
